baf3339b69e6923d137e9e899d096f95b6fb8577
NikhilGithubOfficial <nkmnikhil0009@gmail.com>
2023-05-20 00:17:21 +0530

    create newBook func

--- main.go
+++ main.go
@@ -1,6 +1,10 @@
 package main
 
 import (
+	"crypto/md5"
+	"encoding/json"
+	"fmt"
+	"io"
 	"log"
 	"net/http"
 
@@ -35,6 +39,31 @@ type Blockchain struct {
 }
 
 var Blockchain *Blockchain
+
+func newBook(w http.ResponseWriter, r *http.Request) {
+	var book Book
+
+	if err := json.NewDecoder(r.Body).Decode(&book); err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		log.Printf("could not create: %v", err)
+		w.Write([]byte("could not create new book"))
+		return
+	}
+
+	h := md5.New()
+	io.WriteString(h, book.ISBN+book.PublishDate)
+	book.ID = fmt.Sprintf("%x", h.Sum(nil))
+
+	resp, err := json.MarshalIndent(book, "", " ")
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		log.Printf("could not marshal payload: %v", err)
+		w.Write([]byte("could not save book data"))
+		return
+	}
+	w.WriteHeader(http.StatusOK)
+	w.Write(resp)
+}
 
 func main() {
 	r := mux.NewRouter()
