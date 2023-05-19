87a07687b8fa54b91f81f8c836288fb88a097085
NikhilGithubOfficial <nkmnikhil0009@gmail.com>
2023-05-20 00:01:53 +0530

    define structs

--- main.go
+++ main.go
@@ -7,6 +7,35 @@ import (
 	"github.com/gorilla/mux"
 )
 
+type Block struct {
+	Pos       int
+	Data      BookCheckout
+	TimeStamp string
+	Hash      string
+	PrevHash  string
+}
+
+type Book struct {
+	ID          string `json:"id"`
+	Title       string `json:"title"`
+	Author      string `json:"author"`
+	PublishDate string `json:"publish_date"`
+	ISBN        string `json:"isbn"`
+}
+
+type BookCheckout struct {
+	BookID       string `json:"book_id"`
+	User         string `json:"user"`
+	CheckoutDate string `json:"checkout_date"`
+	IsGenesis    bool   `json:"is_genesis"`
+}
+
+type Blockchain struct {
+	blocks []*Block
+}
+
+var Blockchain *Blockchain
+
 func main() {
 	r := mux.NewRouter()
 	r.HandleFunc("/", getBlockchain).Methods("GET")
